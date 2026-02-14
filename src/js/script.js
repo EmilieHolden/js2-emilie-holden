f849d66e6c271ee0d4ef936a48a1d5f58ab3208a
EmilieHolden <e.393@hotmail.com>
2026-02-14 22:04:37 +0100

    changed link to register.html from index.html from <button> to <a>

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,4 @@ import "./login.js";
 import "./feed.js";
 import "./register.js";
 
-const registerBtn = document.getElementById("registerBtn")
-
-if (registerBtn) {
-    registerBtn.addEventListener("click", () => {
-        window.location.href = "src/pages/register.html"
-    })
-}
-
 
