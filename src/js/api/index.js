1b554c4ed09f41f9e2a56a8774083feca7bd7710
EmilieHolden <e.393@hotmail.com>
2026-02-19 22:43:31 +0100

    updated options variable to options function to get headers. Defined the BASE_URL to clean up code in other files.

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,10 +1,18 @@
 
 const apiKey = '0687c3e4-578f-46b6-8524-17fe91d9bee6';
-const accessToken = localStorage.getItem("token");
+export const BASE_URL = "https://v2.api.noroff.dev/social"
 
+export function getHeaders() {
+    const accessToken = localStorage.getItem("token");
+
+    return {
+        Authorization: `Bearer ${accessToken}`,
+        'X-Noroff-API-Key': apiKey,
+        "Content-Type": "application/json"
+    }
+}
 export const options = {
     headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'X-Noroff-API-Key': apiKey
+
     }
 };
