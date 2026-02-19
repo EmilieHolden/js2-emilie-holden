69cb68ccbd5f99f1acf75c7d734ec9f94eb1ec45
EmilieHolden <e.393@hotmail.com>
2026-02-19 22:37:28 +0100

    added functionality for searching through posts from api

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,77 +1,106 @@
-import { getPosts } from "./api/fetchPosts.js";
+import { getPosts, searchPosts } from "./api/fetchPosts.js";
+import { setupPagination } from "./components/pagination.js";
 
-const loadMoreButton = document.getElementById("load-more-btn")
+const container = document.querySelector("#container");
+const button = document.getElementById("load-more-btn");
+const searchInput = document.getElementById("feedSearchInput");
 
-let currentPage = 1;
-let isFetching = false;
+if (!container || !button) {
 
-async function fetchAndCreatePosts(page = 1) {
-    const container = document.querySelector("#container")
-    if (!container) return
+} else {
+    let currentPage = 1;
+    let isFetching = false;
 
-    if (isFetching) return
-    isFetching = true;
+    let isSearching = false;
+    let searchQuery = "";
 
-    loadMoreButton.textContent = "Loading...";
-    loadMoreButton.disabled = true;
+    function renderPosts(postsToRender, append = false) {
+        if (!append) container.innerHTML = ""
 
-    try {
-        if (page === 1) {
-            container.innerHTML = "<p>Loading...</p>";
+        if (!postsToRender.length && !append) {
+            container.innerHTML = "<p>No posts found.</p>"
+            return
         }
 
-        const data = await getPosts(page, 10);
-        const posts = data.data
-        const meta = data.meta
-
-        if (page === 1) {
-            container.innerHTML = "";
-        }
-
-        posts.forEach(post => {
+        postsToRender.forEach((post) => {
             container.innerHTML += `
             <div class="card">
-                <div class="post-image-container">
-                    ${post.media?.url
+              <div class="post-image-container">
+                ${post.media?.url
                     ? `<img class="card-image" src="${post.media.url}" alt="${post.media.alt || "Post image"}">`
                     : ""
                 }
+              </div>
+              <div class="card-content">
+                <a href="/src/pages/profile.html?name=${encodeURIComponent(post.author.name)}" class="post-user">${post.author?.name || "Unknown author"}</a>
+                <p class="post-date">${new Date(post.created).toLocaleDateString()}</p>
+                <div class="post-body-container">
+                  <p class="post-body post-body-title">${post.title ?? ""}</p>
+                  <p class="post-body">${post.body ?? ""}</p>
                 </div>
-                <div class="card-content">
-                    <p class="post-user">${post.author?.name || "Unknown author"}</p>
-                     <p class="post-date">${new Date(post.created).toLocaleDateString()}</p>
-                     <div class="post-body-container">
-                     <p class="post-body">${post.body}</p>
-                    </div>
-                </div>
+              </div>
             </div>
-            `;
-        });
-
-        if (meta.isLastPage) {
-            loadMoreButton.style.display = 'none'
-        } else {
-            currentPage = meta.nextPage
-            loadMoreButton.textContent = 'Load More'
-            loadMoreButton.disabled = false;
-        }
-
-    } catch (error) {
-        container.textContent = 'Failed to load posts'
-        console.error(error)
-    } finally {
-        isFetching = false
+          `;
+        })
     }
 
-    loadMoreButton.addEventListener("click", () => {
+    async function loadPostsPage(page = 1) {
         if (isFetching) return;
-        fetchAndCreatePosts(currentPage);
-    });
+        isFetching = true;
 
+        button.textContent = "Loading...";
+        button.disabled = true;
+
+        try {
+            if (page === 1) {
+                container.innerHTML = "<p>Loading...</p>";
+            }
+
+            const data = isSearching
+                ? await searchPosts(searchQuery, page, 10)
+                : await getPosts(page, 10);
+            const posts = data.data;
+            const meta = data.meta;
+
+            if (page === 1) {
+                container.innerHTML = "";
+            }
+
+            // Append posts when page > 1, to prevent posts on previus pages to disappear
+            renderPosts(posts, page !== 1)
+
+
+            if (meta.isLastPage) {
+                button.style.display = "none";
+            } else {
+                currentPage = meta.nextPage;
+                button.textContent = "Load More";
+                button.disabled = false;
+            }
+        } catch (error) {
+            container.textContent = "Failed to load posts";
+            console.error(error);
+        } finally {
+            isFetching = false;
+        }
+    }
+
+    if (searchInput) {
+        searchInput.addEventListener("input", (e) => {
+            searchQuery = e.target.value.trim().toLowerCase()
+            currentPage = 1
+
+            if (!searchQuery) {
+                isSearching = false
+                loadPostsPage(1)
+                return
+            }
+
+            isSearching = true
+            loadPostsPage(1)
+        })
+    }
+
+    setupPagination(button, () => isFetching, () => loadPostsPage(currentPage));
+    loadPostsPage();
 }
-
-fetchAndCreatePosts()
-
-
-
-
