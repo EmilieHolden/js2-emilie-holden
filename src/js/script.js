ab8568b9cbe34f969b9bab13d649faf98f7e0dc6
EmilieHolden <e.393@hotmail.com>
2026-02-20 20:37:11 +0100

    imported delete.js

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ import "./feed.js";
 import "./register.js";
 import "./components/pagination.js";
 import "./components/addPost.js";
-import "./profile.js"
+import "./profile.js";
+import "./api/delete.js";
 
 
