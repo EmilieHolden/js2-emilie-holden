215df38f066782f1f68d07f271c284e24c010ee5
EmilieHolden <e.393@hotmail.com>
2026-02-22 21:43:41 +0100

    added jsdoc for deletePost

--- src/js/api/delete.js
+++ src/js/api/delete.js
@@ -1,5 +1,13 @@
 import { BASE_URL, getHeaders } from "./index.js"
 
+/**
+ * Deletes a post by its id using API.
+ * 
+ * @param {} id - the id of the post to delete.
+ * @returns - a success message for post deleted, and the post is removed from API.
+ * @example deletePost(123)
+ * Post is deleted.
+ */
 export const deletePost = async (id) => {
     const API_URL = `${BASE_URL}/posts/${id}`
 
