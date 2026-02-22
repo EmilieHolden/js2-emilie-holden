3eca79855dda96533c312daf74b538edb194267f
EmilieHolden <e.393@hotmail.com>
2026-02-22 21:21:52 +0100

    cleaned up imports to script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
 import "./login.js";
 import "./feed.js";
 import "./register.js";
+import "./post.js"
+import "./profile.js"
 import "./components/pagination.js";
 import "./components/addPost.js";
-import "./profile.js";
 import "./api/delete.js";
+import "./api/edit.js";
+import "./components/header.js"
+
 
 
