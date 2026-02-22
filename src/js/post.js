5d50b8eb936c045addb56b208007aa4aad92e021
EmilieHolden <e.393@hotmail.com>
2026-02-22 22:01:17 +0100

    jsdoc for fetchAndShowPost()

--- src/js/post.js
+++ src/js/post.js
@@ -3,25 +3,31 @@ import { getPost } from "./api/fetchPosts";
 const id = new URLSearchParams(window.location.search).get("id")
 const postContainer = document.querySelector("#post-details");
 
+/**
+ * Fetches a post from API using id. Renders post details to the page. 
+ * @returns Updated page with post details. Loading message while data is loading. If fetch request fails it shows a error message for "Failed to load post."
+ * @example fetchAndShowPost()
+ * Post is displayed on page if succeed.
+ */
 async function fetchAndShowPost() {
-    if (!postContainer) return
+  if (!postContainer) return
 
-    let loading = false
+  let loading = false
 
-    try {
-        loading = true
-        postContainer.innerHTML = `
+  try {
+    loading = true
+    postContainer.innerHTML = `
     <span>Loading...</span>
     `
-        const data = await getPost(id)
-        const post = data.data
+    const data = await getPost(id)
+    const post = data.data
 
-        postContainer.innerHTML = `<div class="card" id="post-card">
+    postContainer.innerHTML = `<div class="card" id="post-card">
               <div class="post-image-container">
                 ${post.media?.url
-                ? `<img class="card-image" src="${post.media.url}" alt="${post.media.alt || "Post image"}">`
-                : ""
-            }
+        ? `<img class="card-image" src="${post.media.url}" alt="${post.media.alt || "Post image"}">`
+        : ""
+      }
               </div>
               <div class="card-content">
                 <a href="/src/pages/profile.html?name=${encodeURIComponent(post.author?.name)}" class="post-user">${post.author?.name || "Unknown author"}</a>
@@ -34,10 +40,10 @@ async function fetchAndShowPost() {
                 </div></div>
             </div>  
           `
-    } catch (error) {
-        postContainer.textContent = "Failed to load post";
-        console.error(error);
-    }
+  } catch (error) {
+    postContainer.textContent = "Failed to load post";
+    console.error(error);
+  }
 }
 
 fetchAndShowPost()
